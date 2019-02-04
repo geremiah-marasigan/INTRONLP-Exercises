f65b8ad5762249c4869fb9c668634dc7d0a64330
Zach <geremiah_marasigan@dlsu.edu.ph>
2019-02-04 23:32:46 +0800

    Added Minimum Edit Distance MED function

--- Language Models/For the dudes/Bigram Model.py
+++ Language Models/For the dudes/Bigram Model.py
@@ -2,6 +2,49 @@ from collections import Counter
 import re
 import os
 
+
+def minimum(str1, str2):
+    str1len = len(str1)
+    str2len = len(str2)
+
+    array = [[0 for x in range(str2len+1)] for y in range(str1len+1)]
+
+    for x in range(str1len+1):
+        array[x][0] = x
+
+    for x in range(str2len+1):
+        array[0][x] = x
+
+    num1 = 0
+    num2 = 0
+    num3 = 0
+
+    #Creating table
+    for x in range(1,str1len+1):
+        for y in range(1,str2len+1):
+            if(str1[x-1] == str2[y-1]):
+                num1 = array[x-1][y-1]
+            else:
+                num1 = array[x-1][y-1] + 2 #S
+            num2 = array[x][y-1]+1 #I
+            num3 = array[x-1][y]+0 #D
+
+            if (num1 <= num2) and (num1 <= num3):
+               array[x][y] = num1
+            elif (num2 <= num1) and (num2 <= num3):
+               array[x][y] = num2
+            else:
+               array[x][y] = num3      
+
+#    print("-------------------------------")
+#    print("Distance: " + str(array[str1len][str2len]))
+#    print("-------------------------------")
+    return array[str1len][str2len]
+
+
+
+
+
 print("Loading corpus...")
 re_pattern = r'\b[a-zA-Z0-9\-\'\*]+\b|[\.\?\!]'
 directory = [
